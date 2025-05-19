ddb3259b9ca41930990b9ed1763747125048594d
dehuihu <dehuihu@gmail.com>
2025-05-19 11:29:35 -0700

    Co-authored-by: Rachana  <rachanapatel@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -12,9 +12,15 @@
 
 // animals = ["tiger", "panda", "raccoon"]
 // animals.append("opossum")
+let animals = ['tiger', 'panda', 'raccoon'];
+animals.push('opposum');
+console.log(animals);
 
 // dog = {"name": "Sprinkles", "age": 3}
 // dog["is_friendly"] = True
+let dog = {'name': 'Sprinkles', 'age': 3};
+dog['is.friendly'] = true;
+console.log(dog);
 
 //
 //
@@ -25,13 +31,41 @@
 // num = 5
 // for i in range(num):
 //    print(i)
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+};
+
 
 // for animal in animals:
 //    print(animal)
+for (let i = 0; i < animals.length; i++) {
+  console.log(animals[i]);
+};
+
+for (const animal of animals) {
+  console.log(animal);
+}
+
+animals.forEach(function(animal) {
+  console.log(animal);
+});
+
+// for
+console.log('Using for/in loop method');
+for (const index in animals) {
+  console.log(animals[index]);
+}
 
 // for k, v in dog.items():
 //    print(f"{k}: {v}")
-
+console.log('for Object entries');
+for (const [key, value] of Object.entries(dog)) {
+  console.log(`${key}: ${value}`);
+}
+console.log('Using Object.entries forEach method');
+Object.entries(dog).forEach(([key, value]) => {
+  console.log(`${key}: ${value}`);
+});
 //
 //
 // ===================================
@@ -49,10 +83,39 @@
 //        return num
 
 // Old way: Function Declaration
-
+// function fizzbuzz(num) {
+//   if (num % 3 === 0 && num % 5 === 0) {
+//     return 'fizzbuzz';
+//   } else if (num % 3 === 0) {
+//     return 'fizz';
+//   } else if (num % 5 === 0) {
+//     return 'buzz';
+//   } else {
+//     return num;
+//   }
+// };
+// console.log(fizzbuzz(1));
+// console.log(fizzbuzz(3));
+// console.log(fizzbuzz(5));
+// console.log(fizzbuzz(15));
 //
 // ES6 way: Function Expression
-
+const fizzbuzz = function(num) {
+  if (num % 3 === 0 && num % 5 === 0) {
+    return 'fizzbuzz';
+  } else if (num % 3 === 0) {
+    return 'fizz';
+  } else if (num % 5 === 0) {
+    return 'buzz';
+  } else {
+    return num;
+  }
+};
+console.log('using function expression')
+console.log(fizzbuzz(1));
+console.log(fizzbuzz(3));
+console.log(fizzbuzz(5));
+console.log(fizzbuzz(15));
 //
 //
 // ======================
@@ -67,6 +130,18 @@
 //    def __str__(self):
 //        return f"{self.name} is a {self.species}"
 
+class Animal {
+  constructor(species, name) {
+    this.species = species;
+    this.name = name;
+  }
+
+  toString() {
+    return `${this.name} is a ${this.species}`;
+  }
+}
+// const animal = new Animal('test', 'test-name');
+// console.log(animal);
 // class Dog(Animal):
 //    def __init__(self, name, trick):
 //        super().__init__(species="dog", name=name)
@@ -74,3 +149,17 @@
 //
 //    def __str__(self):
 //        return f"{super().__str__()} that can {self.trick}"
+class Dog extends Animal {
+  constructor(name, trick) {
+    super('Dog', name);
+    this.trick = trick;
+  }
+  toString(){
+    return `${super.toString()} that can ${this.trick}`;
+  }
+}
+
+const myDog = new Dog('Max', 'roll-over');
+console.log(myDog);
+console.log(myDog.toString());
+
